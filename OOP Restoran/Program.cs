7726f8cd5b248cd8b0a63d549cc6cb5439f74d66
otabek-t <totabek006@gmail.com>
2024-11-20 22:08:40 +0300

    OOP Restaurant

---
+++ OOP Restoran/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace OOP_Restoran
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Restourant> list = new List<Restourant>();
+
+            Restourant FirstMenu = new Restourant();
+
+            FirstMenu.FoodName = "Sho'rva";
+            FirstMenu.HowMuch = 350;
+            FirstMenu.Price = 25000;
+            FirstMenu.TakeAWay = "yes or no";
+            FirstMenu.InTheHall = "yes or no";
+
+
+            Restourant SecondMenu = new Restourant();
+
+            SecondMenu.FoodName = "Osh";
+            SecondMenu.HowMuch = 500;
+            SecondMenu.Price = 35000;
+            SecondMenu.TakeAWay = "yes or no";
+            SecondMenu.InTheHall = "yes or no";
+
+
+            FirstMenu.DisplayInfo();
+            SecondMenu.DisplayInfo();
+
+
+        }
+    }
+}
---
+++ OOP Restoran/Restourant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Restoran;
+
+internal class Restourant
+{
+    // proporty
+    public string FoodName { get; set; }
+    public int HowMuch { get; set; }
+    public int Price { get; set; }
+    public string TakeAWay { get; set; }
+    public string InTheHall { get; set; }
+    public void DisplayInfo()
+    {
+    string result = $"Food name: {FoodName}How much: {HowMuch}, Price: {Price}, Take a way: {TakeAWay}, In the Hall: {InTheHall}";
+    Console.WriteLine(result);
+    }
+    
+}
+
