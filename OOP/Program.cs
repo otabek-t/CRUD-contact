a8b9711d2c6057939669246a74aa7503cf979758
otabek-t <totabek006@gmail.com>
2024-11-20 15:03:48 +0300

    OOP Animals

---
+++ OOP/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace g10;
+
+internal class Program
+{
+
+    static void Main(string[] texts)
+    {
+
+        List<Animal> animals = new List<Animal>();
+
+        Animal car1 = new Animal();
+        car1.Type = "Tiger";
+        car1.Color = "Red and black";
+        car1.Speed = 60;
+        car1.Weight = 150;
+        car1.PlaceOfResidence = "Forest and Jungle";
+
+        car1.DisplayInfo();
+        
+        
+    }
+}
---
+++ OOP/Students.cs
@@ -0,0 +1,24 @@
+﻿namespace g10;
+
+public class Animal
+{
+   
+    public string Type { get; set; }
+
+    public string Color { get; set; } 
+
+    public double Speed { get; set; }
+
+    public string Feet { get; set; }
+
+    public int Weight { get; set; }
+
+    public string PlaceOfResidence { get; set; }
+
+
+    public void DisplayInfo()
+    {
+        string res = $"Name : {Type},  Color : {Color}, Speed : {Speed}, Weight : {Weight}, Place of residence : {PlaceOfResidence}";
+        Console.WriteLine(res);
+    }
+}
